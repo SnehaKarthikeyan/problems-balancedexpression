2cdc5defddf2dc4927a80459ddaddbb0601cad4b
SnehaKarthikeyan <79981351+SnehaKarthikeyan@users.noreply.github.com>
2021-03-03 20:18:24 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,28 @@
+Coding:
+
+def balanced(expr): 
+    stack = [] 
+    for char in expr: 
+        if char in ["(", "{", "["]: 
+            stack.append(char) 
+        else:
+            if not stack: 
+                return False
+            current_char = stack.pop() 
+            if current_char == '(': 
+                if char != ")": 
+                    return False
+            if current_char == '{': 
+                if char != "}": 
+                    return False
+            if current_char == '[': 
+                if char != "]": 
+                    return False
+    if stack: 
+        return False
+    return True
+expr = input();
+if balanced(expr): 
+        print("YES") 
+else: 
+        print("NO") 
